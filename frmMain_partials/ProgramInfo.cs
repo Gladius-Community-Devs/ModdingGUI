2596d833a11b854e3d9daad80f0e3c940f46c270
JR <jrbuda01@gmail.com>
2025-05-08 19:36:08 -0700

    forgot to rebuild before I shipped

--- frmMain_partials/ProgramInfo.cs
+++ frmMain_partials/ProgramInfo.cs
@@ -14,7 +14,7 @@ namespace ModdingGUI.Models
         public ProgramInfo()
         {
             // Default values
-            Version = "3.0.0";
+            Version = "3.0.1";
             Author = "BigBuda";
             GitHubUrl = "https://github.com/Gladius-Community-Devs/ModdingGUI/releases";
         }
